a0c79f32315bdd4dd9f22d8a0a9fff323cc52925
TyreszB <tyresz.brash@gmail.com>
2025-06-27 21:11:35 -0400

    created an entity for users

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b4473c2fb64d47d4740e95765494af668377c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+162ee94ddd8b8d42008827899a3c70e886d10961")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Domain/Users.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Domain;
+
+public class Users
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+    public required string Role { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+}
