353cceaf24f4634fe75804edaa989da243adcf43
TyreszB <tyresz.brash@gmail.com>
2025-06-29 20:09:16 -0400

    added a SQL lite DB for testting

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+393d668db48221ded48a9289a51acc3b2113eee3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f13675c9f732d25e4bf6e9d642f9f324d1be363")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
