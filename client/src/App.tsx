1b08b17c19103e9a5aeec76e66d37ff589b4d86e
TyreszB <tyresz.brash@gmail.com>
2025-07-05 15:26:33 -0400

    updated frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,24 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  
+
+  const[user, setUser] = useState<User[]>([]);
+
+  useEffect(() => {
+  axios.get<User[]>('https://localhost:5001/api/user').then((response) => {
+    setUser(response.data);
+  });
+  }, []);
 
   return (
     <>
       <h1>Aircraft Doc App</h1>
+      <ul>
+        {user.map((user) => (
+          <li key={user.id}>{user.username}</li>
+        ))}
+      </ul>
     </>
   )
 }
