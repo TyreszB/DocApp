d6b8056f221c1cededee05ed819623228008d699
TyreszB <tyresz.brash@gmail.com>
2025-07-22 21:46:16 -0400

    added register to user auth service

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5ca34f37136253cb4f79200232b7a46bd4ba385")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc4a542b22acc72723308085dbf2b6274dad6b56")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
