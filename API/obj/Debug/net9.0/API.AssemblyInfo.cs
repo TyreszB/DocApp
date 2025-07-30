043d5699aeaea1406d3985725db4d9f68333a679
TyreszB <tyresz.brash@gmail.com>
2025-07-29 20:58:25 -0400

    working on fetching user

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e602834acce75bbea4ed0e0a837d26cea1da6dec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+762295d4a05bca5e304ea2ec6f4344cd3ee36908")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
