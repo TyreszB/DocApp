83d59f9eaec1dae9d65b6885cf691d2de2f83ba6
TyreszB <tyresz.brash@gmail.com>
2026-03-12 21:00:59 -0400

    Center login page layout and improve styling

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+448d8f88794fa1db5649b925f40b721f529bec29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb20a2ccc2b56f38f6f160f58e13ea000c2c142c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
