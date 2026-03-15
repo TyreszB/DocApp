9a076b5ead8fc3f1f4be5d0aa036ea4f978d8652
TyreszB <tyresz.brash@gmail.com>
2026-03-14 23:46:49 -0400

    fixed issue with styling finnaly back at it

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 const NavBar = () => {
   return (
     <nav className="navbar w-full flex justify-center items-center p-4">
-              <span className="text-xl font-bold text-gray-800">DocApp</span>
+              <span className="text-xl font-bold text-gray-800">DocumentationApp</span>
     </nav>
   );
 };
